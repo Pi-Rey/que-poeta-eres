87bd3b97462cc1f00596e08f2299f549806bf21a
Pi Rey <pilar.rrdgz@gmail.com>
2024-06-14 08:47:51 +0200

    update keyword

--- src/components/Result.jsx
+++ src/components/Result.jsx
@@ -5,15 +5,13 @@ import "../styles/Result.scss";
 function Result({ answerSum, poemsList }) {
   const [keyword, setKeyword] = useState("");
 
-  //FIXME: revisar las keywords, que ahora son 'soledad', 'libertad', "mujer", "futuro", "amor"
-
   const assigningKeyword = () => {
     if (answerSum < 11) {
       return "amor";
     } else if (answerSum >= 11 && answerSum < 21) {
       return "libertad";
     } else if (answerSum >= 21 && answerSum < 31) {
-      return "mujer";
+      return "desafio";
     } else if (answerSum >= 31 && answerSum < 41) {
       return "futuro";
     } else if (answerSum >= 41) {
@@ -42,25 +40,30 @@ function Result({ answerSum, poemsList }) {
     return obj.find((item) => item.keyword === keyword);
   };
   const poem = findPoem(authorObject.poems);
-  console.log(poem);
-  console.log(answerSum + " " + keyword);
+  //   console.log(poem);
+  //   console.log(answerSum + " " + keyword);
 
   //TODO: preguntar por qué funciona con uno y no con el otro
   //hacer la maquetación
   return (
     <div className="divsection">
-    <section className="sectionresult">
-      <h4>Te ha tocado un poema de: <span className="sectionresult__name">{authorObject.author}</span></h4>
-      
-      <h5 className="sectionresult__title">{poem
-          ? poem.title
-          : "No se encontró un poema con la palabra clave deseada."}</h5>
-      <p className="sectionresult__poem">
-        {poem
-          ? poem.text
-          : "No se encontró un poema con la palabra clave deseada."}
-      </p>
-    </section>
+      <section className="sectionresult">
+        <h4>
+          Te ha tocado un poema de:{" "}
+          <span className="sectionresult__name">{authorObject.author}</span>
+        </h4>
+
+        <h5 className="sectionresult__title">
+          {poem
+            ? poem.title
+            : "No se encontró un poema con la palabra clave deseada."}
+        </h5>
+        <p className="sectionresult__poem">
+          {poem
+            ? poem.text
+            : "No se encontró un poema con la palabra clave deseada."}
+        </p>
+      </section>
     </div>
   );
 }
